7d560a13268ca6a18d6d1984763030c95d2b78cd
Jay002 <jayesh2598@gmail.com>
2020-10-21 01:08:42 +0530

    Added: CensusAnalyser and IndiaCensus classes to load data from CSV file.

--- main/java/com/capg/census/CensusAnalyser.java
+++ main/java/com/capg/census/CensusAnalyser.java
@@ -1,9 +1,32 @@
 package com.capg.census;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class CensusAnalyser {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Indian States Census Analyser Problem!");
+	public int loadIndiaCensusData(String CSVFilePath) {
+		int numOfEntries = 0;
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(CSVFilePath));
+			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndiaCensusCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+			while(censusCSVIterator.hasNext()) {
+				numOfEntries++;
+				IndiaCensusCSV censusData = censusCSVIterator.next();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return numOfEntries;
 	}
-
 }
---
+++ test/java/com/capg/census/CensusAnalyserTest.java
@@ -0,0 +1,17 @@
+package com.capg.census;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+	
+	private final String INDIA_CENSUS_CSV_FILE = "D:\\Eclipse Workspace\\IndianCensusAnalyser\\src\\test\\resources\\IndiaStateCensusData.csv";
+	
+	@Test
+	public void givenIndianCensusCSVFileShouldReturnCorrectRecords() {
+		CensusAnalyser censusAnalyser = new CensusAnalyser();
+		int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE);
+		assertEquals(29, numOfRecords);
+	}
+}
