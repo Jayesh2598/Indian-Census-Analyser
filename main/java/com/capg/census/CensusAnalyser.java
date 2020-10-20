d5f36bc8a9af49503a4021f5b968022eb693791e
Jay002 <jayesh2598@gmail.com>
2020-10-20 22:01:14 +0530

    Start: Welcome message displayed.

---
+++ main/java/com/capg/census/CensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.capg.census;
+
+public class CensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyser Problem!");
+	}
+
+}
