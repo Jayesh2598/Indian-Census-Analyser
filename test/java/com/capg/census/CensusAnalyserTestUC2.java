9beba3901c7c4333e9a33d98bc6355acd7f24211
Jay002 <jayesh2598@gmail.com>
2020-10-21 10:28:38 +0530

    Added: Test case to verify if custom exception is thrown when incorrect csv file path is entered

--- test/java/com/capg/census/CensusAnalyserTestUC2.java
+++ test/java/com/capg/census/CensusAnalyserTestUC2.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 public class CensusAnalyserTestUC2 {
 	
 	private static final String INDIA_STATE_CODE = "D:\\Eclipse Workspace\\IndianCensusAnalyser\\src\\test\\resources\\IndiaStateCode.csv";
+	private static final String WRONG_CSV_FILE_PATH = "D:\\Eclipse Workspace\\IndianCensusAnalyser\\src\\main\\resources\\IndiaStateCode.csv";
 	
 	@Test
 	public void givenIndiaStateCodeFileShouldReturnCorrectRecords() {
@@ -21,5 +22,18 @@ public class CensusAnalyserTestUC2 {
 			e.printStackTrace();
 		}
 		assertEquals(37, numOfRecords);
+	}
+	
+	@Test
+	public void givenIndianStateCodeCSVFileIfIncorrectReturnsCustomException() {
+		CensusAnalyser censusAnalyser = new CensusAnalyser();
+		ExpectedException exception = ExpectedException.none();
+		exception.expect(CensusAnalyserException.class);
+		try {
+			censusAnalyser.loadIndiaStatesCode(WRONG_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+			assertEquals(CensusAnalyserException.ExceptionType.IncorrectCensusFile, e.type);
+		}
 	}
 }
